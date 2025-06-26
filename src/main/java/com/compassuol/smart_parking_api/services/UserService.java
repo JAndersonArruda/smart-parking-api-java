bc217b529cfd1d3e5c9f51b785152596221ee4ce
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-06-26 16:26:03 -0300

    feat: add finaAll and findById User endpoints

--- src/main/java/com/compassuol/smart_parking_api/services/UserService.java
+++ src/main/java/com/compassuol/smart_parking_api/services/UserService.java
@@ -6,8 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
+import java.util.List;
+
 @RequiredArgsConstructor
+@Service
 public class UserService {
 
     private final UserRepository userRepository;
@@ -15,5 +17,17 @@ public class UserService {
     @Transactional
     public User save(User user) {
         return userRepository.save(user);
+    }
+
+    @Transactional
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("User not found.")
+        );
     }
 }
--- src/main/java/com/compassuol/smart_parking_api/web/controllers/UserController.java
+++ src/main/java/com/compassuol/smart_parking_api/web/controllers/UserController.java
@@ -5,10 +5,10 @@ import com.compassuol.smart_parking_api.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +21,17 @@ public class UserController {
     public ResponseEntity<User> create(@RequestBody User user) {
         User newUser = userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return  ResponseEntity.ok(user);
     }
 }
