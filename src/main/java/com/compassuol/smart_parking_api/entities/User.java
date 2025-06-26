e08ef0769a4e83cd71c5af5627535f163ed2e63e
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-06-26 14:25:30 -0300

    feat: add User entity

---
+++ src/main/java/com/compassuol/smart_parking_api/entities/User.java
@@ -0,0 +1,67 @@
+package com.compassuol.smart_parking_api.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "tb_users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    public enum Role {
+        ROLE_ADMIN,
+        ROLE_CLIENT
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                '}';
+    }
+}
